6947e1084b4a67438ea6c13b3b79e2ee1093f569
wimos-ai <mosierwilliam6@gmail.com>
2025-11-19 16:01:43 -0600

    Added up to date runner and dealer. Added test function to blackjack.sh

--- blackjack.sh
+++ blackjack.sh
@@ -6,6 +6,19 @@ set -euo pipefail
 
 BUILD_DIR="build"
 
+# --- Runner Cfg -------------------------------------------------------------------
+
+RUNNER_EXE="bin/blackjack_runner.out"
+DEALER_SO="bin/libblack_jack_solver_dealer.so"
+PLAYER_SO=$(find ./build/players -type f -name "*.so")
+DUMP_JSON="out.json"
+NDECKS="2"
+RESHUFFLE_PCTG="0.5"
+NROUNDS="1000"
+STARTING_CHIPS="500"
+TBL_MIN="5"
+TBL_MAX="50"
+
 # ------------------------------------------------------------------------------
 
 cmd="${1:-}"
@@ -29,12 +42,29 @@ case "$cmd" in
     cmake --build "$BUILD_DIR"
     ;;
 
+  test)
+    if ! command -v cmake &> /dev/null; then
+      echo "Error: cmake not found. Please install CMake."
+      exit 1
+    fi
+    "$RUNNER_EXE" \
+    --dealer "$DEALER_SO" \
+    --player "$PLAYER_SO" \
+    --output "$DUMP_JSON" \
+    --ndecks "$NDECKS" \
+    --reshuffle "$RESHUFFLE_PCTG" \
+    --rounds "$NROUNDS" \
+    --starting "$STARTING_CHIPS" \
+    --min "$TBL_MIN" \
+    --max "$TBL_MAX"
+  ;;
+
   clean)
     rm -rf "$BUILD_DIR"
     ;;
 
   *)
-    echo "Usage: $0 {build|debug|clean}"
+    echo "Usage: $0 {build|debug|clean|test}"
     exit 1
     ;;
 esac
